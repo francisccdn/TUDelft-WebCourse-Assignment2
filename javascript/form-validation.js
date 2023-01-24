175b88dd473ca0ce66be2738cd68d1b5d12d7526
Francisco Cunha <francisccdn@gmail.com>
2023-01-24 11:37:02 +0100

    javascript validation for some fields

--- javascript/form-validation.js
+++ javascript/form-validation.js
@@ -1,6 +1,8 @@
 const VALID_INPUT = "valid";
 const validInputMsg = "Looks good!";
 
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTUVWXYZ";
+
 const formEl = document.getElementById("signupform")
 const inputFieldsParentEl = document.getElementById("sign-up-inputs")
 
@@ -10,22 +12,81 @@ const fieldRules = {
             return "Please input a username";
         }
 
+        if (value.length < 5) {
+            return "Username must be at least 5 characters";
+        }
+
+        if (value.length > 12) {
+            return "Username must be at most 12 characters";
+        }
+
+        const firstChar = value.charAt(0);
+        if (firstChar !== firstChar.toUpperCase()) {
+            return "First character must be uppercase";
+        }
+
+        const lastChar = value.charAt(value.length - 1);
+        if (alphabet.includes(lastChar)) {
+            return "Last character must be number or special character";
+        }
+
         return VALID_INPUT;
     },
-    "Password" : (value) => {return VALID_INPUT;},
-    "Name" : (value) => {return VALID_INPUT;},
+    "Password" : (value) => {return VALID_INPUT;}, // TODO
+    "Name" : (value) => {return VALID_INPUT;}, // TODO
     "Address" : (value) => {return VALID_INPUT;},
-    "Country" : (value) => {return VALID_INPUT;},
-    "ZIP code" : (value) => {return VALID_INPUT;},
-    "Email" : (value) => {return VALID_INPUT;},
-    "Sex" : (value) => {return VALID_INPUT;},
-    "Language" : (value) => {return VALID_INPUT;},
+    "Country" : (value) => (value) => {
+        if (value == "" || value == null) {
+            return "This is a required field";
+        }
+
+        return VALID_INPUT;
+    },
+    "ZIP code" : (value) => {
+        if (value == "" || value == null) {
+            return VALID_INPUT;
+        }
+
+        if (value.length != 6) {
+            return "Not a valid ZIP code";
+        }
+
+        for (let i = 0; i < 4; i++) {
+            const char = value.charAt(i);
+            
+            if (isNaN(parseInt(char))) {
+                return "First four characters must be digits";
+            }
+        }
+        for (let i = 4; i < 6; i++) {
+            const char = value.charAt(i);
+            
+            if (!alphabet.includes(char)) {
+                return "Last two characters must be letters";
+            }
+        }
+    
+        return VALID_INPUT;
+    },
+    "Email" : (value) => {return VALID_INPUT;}, // TODO
+    "Sex" : (value) => {
+        if (value == "" || value == null) {
+            return "This is a required field";
+        }
+
+        return VALID_INPUT;
+    },
+    "Language" : (value) => (value) => {
+        if (value == "" || value == null) {
+            return "This is a required field";
+        }
+
+        return VALID_INPUT;
+    },
     "Bio" : (value) => {return VALID_INPUT;},
     "Reason of use" : (value) => {return VALID_INPUT;},
     "A fun joke" : (value) => {return VALID_INPUT;},
 }
-
-console.log(formEl);
 
 formEl.addEventListener("submit", (e) => {
     let invalidInput = false;
